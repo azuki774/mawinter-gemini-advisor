22bb6b0786a802a2a5acd0c9d990b87961ea7ec7
azuki774 <azuki774s@gmail.com>
2025-05-16 00:55:56 +0900

    add mawinter client

---
+++ internal/mawinter/getMonthlyData.go
@@ -0,0 +1,32 @@
+package mawinter
+
+import (
+	"io"
+	"net/http"
+)
+
+type MawinterClient struct {
+	Endpoint string // ex. http://localhost:8080/v2/record
+}
+
+func NewMawinterClient(endpoint string) *MawinterClient {
+	return &MawinterClient{
+		Endpoint: endpoint,
+	}
+}
+
+func (m *MawinterClient) GetMonthlyData(yyyymm string) (string, error) {
+	endpoint := m.Endpoint + "&yyyymm=" + yyyymm
+	res, err := http.Get(endpoint)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
